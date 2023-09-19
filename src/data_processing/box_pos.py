e424d6889894fd7c0d7e960cbb2242fc9ecba5cb
Andrew <88972912+Slothman1@users.noreply.github.com>
2023-09-19 22:35:04 +1000

    Finished box_pos.py

--- src/data_processing/box_pos.py
+++ src/data_processing/box_pos.py
@@ -9,16 +9,40 @@ def main():
 # checks if the user input is between a certain range and returns appropriate position
 
 def check_x_pos(x1,x2):
-	if x1 >= 0 and x2 <= 46:
+	if x1 >= 4 and x2 <= 32:
 		return 1
+	if x1 >= 61 and x2 <= 81:
+		return 2
+	if x1 >= 110 and x2 <= 130:
+		return 3
+	if x1 >= 160 and x2 <= 177:
+		return 4
+	if x1 >= 209 and x2 <= 228:
+		return 5
+	if x1 >= 258 and x2 <= 274:
+		return 6
+	if x1 >= 307 and x2 <= 324:
+		return 7
+	if x1 >= 356 and x2 <= 376:
+		return 8
 	else:
-		return 0
+		print("Error: x1 and x2 are not within range")
 
 def check_y_pos(y1,y2):
+	if y1 >= 0 and y2 <= 40:
+		return 0
+	if y1 >= 39 and y2 <= 76:
+		return 1
+	if y1 >= 75 and y2 <= 113:
+		return 2
+	if y1 >= 112 and y2 <= 151:
+		return 3
+	if y1 >= 150 and y2 <= 189:
+		return 4
 	if y1 >= 184 and y2 <= 223:
 		return 5
 	else:
-		return 0
+		print("Error: y1 and y2 are not within range")
 
 # the defined calculation for the box number
 def return_num(x1, y1, x2, y2):
