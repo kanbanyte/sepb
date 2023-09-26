66c18b48294f27d0a5dd279989a3f463be226cb4
Andrew <88972912+Slothman1@users.noreply.github.com>
2023-09-26 15:13:49 +1000

    Added move checks

--- src/samples/python/Tray_Pos.py
+++ src/samples/python/Tray_Pos.py
@@ -1,15 +1,24 @@
-tray1 = "empty"
-tray2 = "empty"
-assembly = "empty"
+tray1 = False
+tray1_move = False
+tray2 = False
+tray2_move = False
+assembly = False
+assembly_move = False
 
 # checks if bounding box is between a certain range and returns a corresponding position
-def get_position_from_bounding_box(x1, y1, x2, y2):
+def get_position_from_bounding_box(x1, y1, x2, y2, tray_class):
 	if x1 >=0  and y1 >= 140 or x2 <= 335 or y2 <= 400:
-		assembly = "has tray"
+		assembly = True
+		if tray_class == "empty":
+			assembly_move = True
 	if x1 >= 340 and y1 >= 0 or x2 <= 672 or y2 <= 265:
-		tray2 = "has tray"
+		tray2 = True
+		if tray_class == "full":
+			tray2_move = True
 	if x1 >= 340 and y1 >= 288 or x2 <= 685 or y2 <= 575:
-		tray1 = "has tray"
+		tray1 = True
+		if tray_class == "full":
+			tray1_move = True
 
 #determine class and if the tray needs to be moved
 def tray_class_move(x1, y1, x2, y2, Tray_class):
@@ -54,5 +63,23 @@ def tray_class_move(x1, y1, x2, y2, Tray_class):
 		return "move to assembly"
 	if switch_t2(_temp) == "move to assembly" and switch_assembly(_temp) == "clear":
 		return "move to assembly"
+
+
+def check_move(tray_class):
+	if assembly == "empty":
+		if tray1 == "full":
+			tray1 = "empty"
+			return "move to assembly"
+		elif tray2 == "full":
+			tray2 = "empty"
+			return "move to assembly"
+	if assembly == "has tray" and tray_class == "empty":
+		if tray1 == "empty":
+			assembly = "empty"
+			return "move to tray 1"
+		elif tray2 == "empty":
+			assembly = "empty"
+			return "move to tray 2"
+
 
 print(tray_class_move(5, 147, 330, 397, "empty"))
