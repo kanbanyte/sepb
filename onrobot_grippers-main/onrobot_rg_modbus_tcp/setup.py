393b26b97be7b21dd42e6ae6c18e48f0fa465878
finnmcgearey <75763222+finnmcgearey@users.noreply.github.com>
2023-09-27 13:51:42 +1000

    Update onrobot_grippers-main
    Attempted to convert gripper package to ROS2.
    
    Co-authored-by: ~ D <dau501@users.noreply.github.com>

---
+++ onrobot_grippers-main/onrobot_rg_modbus_tcp/setup.py
@@ -0,0 +1,27 @@
+import os
+from glob import glob
+from setuptools import find_packages, setup
+
+package_name = 'onrobot_rg_modbus_tcp'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='cobot',
+    maintainer_email='75763222+finnmcgearey@users.noreply.github.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
