2977d1dcebf5c5c319af6c43f778437b9ba8e60d
Andrew <88972912+Slothman1@users.noreply.github.com>
2023-09-27 21:47:30 +1000

    added enums and better returns

--- src/data_processing/Tray_Pos.py
+++ src/data_processing/Tray_Pos.py
@@ -1,3 +1,5 @@
+from enum import Enum
+
 dict = {
 	"assembly": False,
 	"assembly_move": False,
@@ -7,6 +9,17 @@ dict = {
 	"tray2_move": False
 }
 
+class TrayPos(Enum):
+	tray1 = 1
+	tray2 = 2
+	assembly = 3
+
+class TrayMovement(Enum):
+	move_assembly_tray1 = 1
+	move_assembly_tray2 = 2
+	move_tray1_assembly = 3
+	move_tray2_assembly = 4
+
 # checks if bounding box is between a certain range and returns a corresponding position
 def get_position_from_bounding_box(bounding_box, tray_class):
 	x1, y1, x2, y2 = bounding_box
@@ -14,17 +27,17 @@ def get_position_from_bounding_box(bounding_box, tray_class):
 		dict.update({"assembly": True})
 		if tray_class.lower() == "empty":
 			dict.update({"assembly_move": True})
-		return None
+		return TrayPos.assembly
 	if x1 >= 340 and y1 >= 0 or x2 <= 672 or y2 <= 265:
 		dict.update({"tray2": True})
 		if tray_class.lower() == "full":
 			dict.update({"tray2_move": True})
-		return None
+		return TrayPos.tray2
 	if x1 >= 340 and y1 >= 288 or x2 <= 685 or y2 <= 575:
 		dict.update({"tray1": True})
 		if tray_class.lower() == "full":
 			dict.update({"tray1_move": True})
-		return None
+		return TrayPos.tray1
 
 # checks if the trays are in a moveable state and returns a corresponding move
 def check_move():
@@ -32,20 +45,20 @@ def check_move():
 		if dict.get("tray1_move") == True:
 			dict.update({"tray1": False})
 			dict.update({"tray1_move": False})
-			return "move tray 1 to assembly"
+			return TrayMovement.move_tray1_assembly
 		elif dict.get("tray2_move") == True:
 			dict.update({"tray2_move": False})
 			dict.update({"tray2": False})
-			return "move tray 2 to assembly"
+			return TrayMovement.move_tray2_assembly
 	if dict.get("assembly") == True and dict.get("assembly_move") == True:
 		if dict.get("tray1") == False:
 			dict.update({"assembly": False})
 			dict.update({"assembly_move": False})
-			return "move from assembly to tray 1"
+			return TrayMovement.move_assembly_tray1
 		elif dict.get("tray2") == False:
 			dict.update({"assembly": False})
 			dict.update({"assembly_move": False})
-			return "move from assembly to tray 2"
+			return TrayMovement.move_assembly_tray2
 
 # #endregion
 
