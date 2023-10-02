13ee01fbfb583b503a72b551d91ca9206ddf7fae
finnmcgearey <75763222+finnmcgearey@users.noreply.github.com>
2023-10-02 16:51:04 +1100

    Update ws/src/pick_place_interfaces
    Changes:
    * Created Case.srv, Chip.srv, Tray.srv.
    * Modified bot_node.py to work with camera_node.
    
    Co-authored-by: ~Huy-GV <Huy-GV@users.noreply.github.com>

--- ws/src/bot_package/nodes/camera_node.py
+++ ws/src/bot_package/nodes/camera_node.py
@@ -6,6 +6,7 @@ from models.python.detected_object import *
 from data_processing.case_position import *
 from data_processing.tray_position import *
 from data_processing.chip_position import *
+from util.file_reader import *
 
 from pick_place_interfaces.srv import Case
 from pick_place_interfaces.srv import Tray
@@ -22,8 +23,21 @@ class CameraServer(Node):
 		# response.sum = request.a + request.b
 		# self.get_logger().info('Incoming request\na: %d b: %d' % (request.a, request.b))
 
+		config_file_path = "/home/cobot/Documents/models/config.yaml"
+		config = read_yaml(config_file_path)
+		crop_box = read_crop_box(config.get('case_crop_box').get('right'))
+		model = ObjectDetectionModel(config.get('model').get('detect_case'))
+
+		camera = open_camera(config.get('camera'))
+		cropped_image = get_rgb_cropped_image(camera, crop_box, LogicalLens.RIGHT)
+		detections = model.run_inference(cropped_image)
+
+		if len(detections.items()) == 0:
+			response.case_number = -1
+
 		# return response
-		response.case_number = 5
+		detected_case = detections[0][0]
+		response.case_number = convert_case_bounding_boxes(detected_case)
 		self.get_logger().info(f"Request: {request}, Case Number: {response.case_number}")
 
 		return response
