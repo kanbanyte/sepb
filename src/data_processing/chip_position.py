e329ca3ed629247a4207bb8972f1f0897276253b
Andrew <88972912+Slothman1@users.noreply.github.com>
2023-10-08 22:00:21 +1100

    added tests

--- src/data_processing/chip_position.py
+++ src/data_processing/chip_position.py
@@ -10,25 +10,29 @@ def get_chip_col(x1,x2):
 	Returns:
 		int : the column number of the chip, or -1 for invalid column
 	'''
-	xcen = (x1 + x2) / 2 #finds the center of the chips x coords
-	if xcen >= 4 and xcen <= 37:
-		return 0
-	if xcen >= 56 and xcen <= 86:
-		return 1
-	if xcen >= 105 and xcen <= 135:
-		return 2
-	if xcen >= 155 and xcen <= 182:
-		return 3
-	if xcen >= 204 and xcen <= 233:
-		return 4
-	if xcen >= 253 and xcen <= 284:
-		return 5
-	if xcen >= 302 and xcen <= 329:
-		return 6
-	if xcen >= 351 and xcen <= 381:
-		return 7
+	if x1 < x2 and x1 >=0 and x2 >= 0:
+		xcen = (x1 + x2) / 2 #finds the center of the chips x coords
+		if xcen >= 4 and xcen <= 37:
+			return 0
+		if xcen >= 56 and xcen <= 86:
+			return 1
+		if xcen >= 105 and xcen <= 135:
+			return 2
+		if xcen >= 155 and xcen <= 182:
+			return 3
+		if xcen >= 204 and xcen <= 233:
+			return 4
+		if xcen >= 253 and xcen <= 284:
+			return 5
+		if xcen >= 302 and xcen <= 329:
+			return 6
+		if xcen >= 351 and xcen <= 381:
+			return 7
+		else:
+			print(f"X-center out of bounds: {xcen}")
+			return -1
 	else:
-		print(f"X-center out of bounds: {xcen}")
+		print("Invalid X-coordinates")
 		return -1
 
 def get_chip_row(y1,y2):
@@ -43,21 +47,25 @@ def get_chip_row(y1,y2):
 	Returns:
 		int : the row number of the chip, or -1 for invalid row
 	'''
-	ycen = (y1 + y2) / 2 #finds the center of the chips y coords
-	if ycen >= 0 and ycen <= 40:
-		return 0
-	if ycen >= 39 and ycen <= 76:
-		return 1
-	if ycen >= 70 and ycen <= 113:
-		return 2
-	if ycen >= 112 and ycen <= 151:
-		return 3
-	if ycen >= 150 and ycen <= 189:
-		return 4
-	if ycen >= 184 and ycen <= 223:
-		return 5
+	if y1 < y2 and y1 >=0 and y2 >= 0:
+		ycen = (y1 + y2) / 2 #finds the center of the chips y coords
+		if ycen >= 0 and ycen <= 40:
+			return 0
+		if ycen >= 39 and ycen <= 76:
+			return 1
+		if ycen >= 70 and ycen <= 113:
+			return 2
+		if ycen >= 112 and ycen <= 151:
+			return 3
+		if ycen >= 150 and ycen <= 189:
+			return 4
+		if ycen >= 184 and ycen <= 223:
+			return 5
+		else:
+			print(f"Y-center out of bounds: {ycen}")
+			return -1
 	else:
-		print(f"Y-center out of bounds: {ycen}")
+		print("Invalid Y-coordinates")
 		return -1
 
 def get_chip_slot_number(bounding_box):
